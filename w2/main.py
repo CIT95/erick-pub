5ae28f11115ac6bbfa6a3d3660e13a69a8c62566
Eric Kossian <ericssiane@gmail.com>
2022-01-15 03:49:05 -0800

    starting setup complete

---
+++ w2/main.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+# Write your code below this line 👇
+first_number = two_digit_number[0]
+second_number = two_digit_number[1]
+first_int = int(first_number)
+second_int = int(second_number)
+print(first_int + second_int)
